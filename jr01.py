01b2e644d663e6b5e82c9cd12464de1d99325e6d
Jay Berkenbilt <ejb@ql.org>
2022-11-06 15:17:14 -0500

    Change terminology in reconfigure dialog

--- jr01.py
+++ jr01.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+
+# The MIT License (MIT)
+# Copyright © 2022 Jay Berkenbilt <ejb@ql.org>
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the “Software”), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import tkinter
 import tkinter.filedialog
 import math
@@ -289,7 +313,7 @@ class JR01Win:
         if state is None:
             while 1:
                 answer = tkinter.simpledialog.askstring(
-                    "Parameters", "Enter three numbers: bars pegs lights")
+                    "Parameters", "Enter three numbers: rows columns lights")
                 if answer:
                     m = self.params_re.match(answer)
                     if m:
